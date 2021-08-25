806b45960f11b0add06277ac9b019eecb781ad90
Malik Soltukiev <freelance99@bk.ru>
2021-08-25 16:59:44 +0300

    added Page CorporateGovernance

--- src/pages/CorporateGovernance/index.tsx
+++ src/pages/CorporateGovernance/index.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react'
-import { Route, useLocation } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import { TaxManagementPolicy } from '../../modules/CorporateGovernance/TaxManagementPolicy'
 import { RiskManagementPolicy } from '../../modules/CorporateGovernance/RiskManagementPolicy'
 import { AntiBriberyPolicy } from '../../modules/CorporateGovernance/AntiBriberyPolicy'
@@ -8,13 +8,32 @@ import { CodeOfConduct } from '../../modules/CorporateGovernance/CodeOfConduct'
 import './CorporateGovernance.scss'
 
 export const CorporateGovernance: FC = () => {
-  const { pathname } = useLocation()
-
   return (
     <main className='corporate-governance'>
       <div className='corporate-governance__preview'>
         <div className='row row-md'>
-          <h1 className='h1 corporate-governance__preview-title'>EMPEROR HOLDINGS ANTI-BRIBERY POLICY</h1>
+          <h1 className='h1 corporate-governance__preview-title'>
+            <Route
+              exact
+              path='/corporate_governance/anti_bribery_policy'
+              component={() => <span> EMPEROR HOLDINGS ANTI-BRIBERY POLICY</span>}
+            />
+            <Route
+              exact
+              path='/corporate_governance/code_of_conduct'
+              component={() => <span> EMPEROR HOLDINGS CODE OF CONDUCT</span>}
+            />
+            <Route
+              exact
+              path='/corporate_governance/risk_management_policy'
+              component={() => <span>EMPEROR HOLDINGS RISK MANAGEMENT POLICY</span>}
+            />
+            <Route
+              exact
+              path='/corporate_governance/tax_management_policy'
+              component={() => <span> EMPEROR HOLDINGS RESPONSIBLE TAX MANAGEMENT POLICY</span>}
+            />
+          </h1>
         </div>
       </div>
       <div className='lines'>
