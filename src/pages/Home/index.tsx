c875a29418f99638e6a44b8c1febc843bd9de373
seredganyha <karix05@yandex.ru>
2022-09-12 14:52:59 +0300

    drop unnecessary code || AdvisoryBoard

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment} from 'react';
 
-import {AdvisorCard, CompCard, ManagerCard} from '../../components';
+import { CompCard, ManagerCard } from '../../components';
 
 import './Home.scss';
 
@@ -17,13 +17,6 @@ import MImg5 from '../../assets/img/managers/5.jpg';
 import MImg6 from '../../assets/img/managers/6.jpg';
 import MImg7 from '../../assets/img/managers/7.jpg';
 import MImg8 from '../../assets/img/managers/8.jpg';
-import Line from '../../assets/img/advisory/line.png';
-import Adv1 from '../../assets/img/advisory/advisor-1.png';
-import Adv2 from '../../assets/img/advisory/advisor-2.png';
-import Adv3 from '../../assets/img/advisory/advisor-3.png';
-import Adv4 from '../../assets/img/advisory/advisor-4.png';
-import Adv5 from '../../assets/img/advisory/advisor-5.png';
-import Adv6 from '../../assets/img/advisory/advisor-6.png';
 
 const HomePage: React.FC = () => {
   const cards = [
@@ -104,157 +97,6 @@ const HomePage: React.FC = () => {
       name: 'Sanjay Bhoola',
       position: 'Head of Ducatus Property & Travel',
     },
-  ];
-  const advisory = [
-    {
-      name: 'Dr. Hatem Al Shanfari ',
-      position: 'Entrepreneur, Educator and Member of the Board of Governors of the Central Bank of Oman',
-      img: Adv1,
-      bio:
-        <Fragment>
-          One of Oman's leading economists, Dr. Hatem Al-Shanfari is Assistant Professor in the Department of Economics
-          and Finance at the College of Commerce and Economics at Sultan Qaboos University. His research interest is
-          focused on asset pricing models, risk management, financial markets, management of financial institutions and
-          Trade in Services (GATS). He has published a number of articles in referred international journals and
-          participated in a number of professional international conferences.
-          <br/>
-          <br/>
-          Dr. Al Shanfari is the current chairman of the Board of Directors of Gulf Investment Services Company and the
-          Chairman of the Audit Committee of the Board of Al Omaniya Financial Services company; Vice Chairman of the
-          Board of Omani Economic Association; a member of the Economics committee of the Oman Chamber of Commerce and
-          Industry; and research associate of the Economic Research Forum for the Arab Countries, Iran and Turkey.
-          <br/>
-          <br/>
-          Dr. Al Shanfari holds a doctorate degree in Economics from the University of Strathclyde in Glasgow, Scotland.
-          He also earned an MA degree in Economic Planning from the Institute of Social Studies in The Hague, the
-          Netherlands and B.Sc in Engineering from Washington University in St. Louis, Missouri, USA.
-        </Fragment>
-    },
-    {
-      name: 'His Excellency Tan Sri Dato Professor Joseph Adaikalam',
-      position: 'Educationist, Philanthropist and Economic Advisor to the Government of Malaysia',
-      img: Adv2,
-      bio:
-        <Fragment>
-          His Excellency Tan Sri Dato Professor Joseph Adaikalam is an educationist, social entrepreneur and
-          philanthropist par excellance. He is an advisor to the present Government of Malaysia in the area of
-          entrepreneurship education and was advisor to the 2 previous Prime Ministers of Malaysia under the National
-          Economic Advisory Council, Prime Minister's Department.
-          <br/>
-          <br/>
-          As an educationist, His Excellency Tan Sri is a pioneer in the private higher education sector and has over 35
-          years experience in international education. He was also the First Indian Vice Chancellor to be appointed by
-          the Government of Malaysia. As a social entrepreneur and philanthropist. His Excellency has assisted thousands
-          of students from Asia, Africa and Central Asia through the Binary Global Scholarship that he established in
-          1994.
-          <br/>
-          <br/>
-          He has been conferred numerous awards at international and national levels. Among these includes Asia Leading
-          Educator for Entrepreneurship award, SME Platinum Award for Entrepreneurship Excellence, NRI Business
-          Leadership Execellence Award (india) and many more.
-          <br/>
-          <br/>
-          For his tremendous contribution to the Nation, His Excellency was knighted as a Dato (Sir) in 2007 and Dato
-          Paduka in 2015. In 2017, His Majesty The King of Malaysia bestowed the highest title of Tan Sri (Lordship)
-          upon His Excellency for his outstanding contribution internationally as an educator, social entrepreneur and
-          philanthropist.
-        </Fragment>
-    },
-    {
-      name: 'Prof. Paolo Cianchi',
-      position: 'Technology Entrepreneur and Digital Transformation Expert',
-      img: Adv3,
-      bio: <Fragment>
-        Prof. Cianchi is a passionate entrepreneur that has strongly contributed to bring Intelligent Systems and Low
-        Code thinking into mainstream Real-Time Enterprise and business integration through Digital Transformation. He
-        promotes the vision among customers, partners and investors by inventing strategic Distributed Intelligent
-        Systems products and application scenarios based on cutting edge technologies including Payments and Blockchain,
-        IoT, AlgorithMIX (AI-ML and mechanistic math models) and Collaborative UX, all orchestrated through the NEGENTIS
-        process-centric middleware software platform.
-        <br/>
-        <br/>
-        Prof. Cianchi graduated Summa Cum Laude in Electronic Engineering at the University of Florence and holds a PhD
-        in Systems Engineering. He currently serves as CEO and CTO of some startups he has co-founded in the software,
-        digital health, fintech and renewable industries. He is also a CTO of Scrypta Foundation to evolve its
-        blockchain into the next-generation DApp fully fledged operating system.
-        <br/>
-        <br/>
-        Recently, he was appointed as Head of the Engineering Department and executive member of the Academic Board of a
-        private university based in Rome aiming to offer highly qualified education programs to students from abroad
-        (mainly Asia, Africa and MiddleEast).
-      </Fragment>
-    },
-    {
-      name: 'Rashid Al Habtoor',
-      position: 'CEO and President of RKHBC, Al Habtoor Trading Enterprises (HTE) LLC and RKH Investments',
-      img: Adv4,
-      bio:
-        <Fragment>
-          Born into one of the largest business families and raised in Dubai, United Arab Emirates, Mr. Al Habtoor's
-          ability to recognise opportunities and capitalise on them has made him a much sought after Strategic Business
-          Partner. His experience spans a broad range of business portfolios which include Oil & Gas, Construction &
-          Engineering, Automotive, Real Estate, Information Technology, Software Systems, Facilities Management,
-          Communications and Food & Beverage, along with over one hundred Partnerships and Joint Ventures with companies
-          both local and international.
-          <br/>
-          <br/>
-          Mr. Al Habtoor graduated from George Washington University with a Bachelor of Science degree in Operations
-          Research and Computer Science. In the early 80s, he started his first business with a vision to contribute to
-          the developing economy of the United Arab Emirates. Mr. Al Habtoor has received a Leadership Award from the
-          Dubai Chamber of Commerce and Industry and also been honoured by the Dubai Business Council four times, with
-          his last award being for "Strategic Partnership in UAE's developing Economy".
-          <br/>
-          <br/>
-          Mr. Al Habtoor’s vision is to continue the growth of UAE’s Economy by maintaining the focus on Strategic
-          Partnership through various business channels including Business Development of New Projects, Establishing
-          Robust Government Relations & Business Consultation via his company, RKH-BC.com. An advocate of supporting the
-          local community, Mr Al Habtoor's philanthropic endeavors are focused on simultaneously nurturing both local
-          and international charity organisations.
-        </Fragment>
-    },
-    {
-      name: 'Jürgen Kob',
-      position: 'Fintech and Asset Management Consultant',
-      img: Adv5,
-      bio:
-        <Fragment>
-          Jürgen is a consultant with 30 years of experience in the traditional financial industry and asset management.
-          He has worked for a variety of banks in Germany, Luxembourg and Switzerland.
-          <br/>
-          <br/>
-          Educated in Business and Economics in Germany, Jürgen went on to build a prolific career in banking and
-          finance. He has held various positions in European finance institutions, such as M.M.Warburg Bank, Deutsche
-          Bank Schweiz AG (DBS), MediBank AG, Theta AG Wealth Management, among others. Over the past few years, he has
-          added a relevance to his wealth of banking expertise through working directly with companies in the field of
-          Fintech and blockchain/cryptocurrencies.
-          <br/>
-          <br/>
-          Today, he serves companies like Denarius in Crypto Valley Zug as a board member in strategic issues. Jürgen is
-          a member of DFJV - Deutscher Fachjournalisten Verband (German Assocation for Specialized Journalists), FINTECH
-          Circle Institute, London, Bitcoin Association Switzerland, Crypto Valley Association, Crypto Valley Labs Zug
-          and is an advocate of Disruption Disciples. He continues to share his wealth of experience as an adviser and
-          consultant on subjects such as Digital Finance & Digital Leadership, Blockchain & Cryptocurrencies and Wealth
-          Management.
-        </Fragment>
-    },
-    {
-      name: 'Joseph Koren',
-      position: 'Business Leader and Cyber Security Expert',
-      img: Adv6,
-      bio: <Fragment>
-        After 35 years in the jewelry industry as CEO of Nissko Jewelry and Daniel K, Mr. Koren cultivated relationships
-        all over the world, ranging from big chain department store executives to VIP clients to Royal Family Members.
-        He specializes in networking, with a focus on connecting businesses with investors, buyers, and strategic
-        partners. Mr. Koren serves as Board Member in various organizations and charities, namely New York Center for
-        Child Development, Soroka Medical Center, and Foundation for Ethnic Understanding.
-        <br/>
-        <br/>
-        Currently, he's a USA Business Development Representative for Israel-based cyber security company Gyptol. It's
-        the only software in the world that detects and remediates misconfigurations (human errors and default settings)
-        among the organizations assets, which are responsible for approximately 40% of all successful cyber attacks. Mr.
-        Koren is very passionate about the Hi-Tech industry and is excited about the endless possibilities it presents.
-      </Fragment>
-    }
   ];
 
   return (
